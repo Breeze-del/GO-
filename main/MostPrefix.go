4ce4452dacdfdee3312e65a0e12ec1740581b69c
Liqingfeng <562916584@qq.com>
2018-09-16 08:37:55 +0800

    string类方法提交

--- main/MostPrefix.go
+++ main/MostPrefix.go
@@ -28,20 +28,25 @@ const (
 var sin30,cos30 = math.Sin(angle), math.Cos(angle)
 
 func main() {
-	fmt.Printf("<svg xmlns= 'http://www.32.org/2000/svg'" +
-		"style='stroke: grey; fill: white; stroke-width: 0.7'" +
-		"width='%d' height= '%d'>",width,height)
-	for i := 0; i < cells; i++ {
-		for j := 0; j < cells; j++ {
-			ax, ay := Corner(i+1,j)
-			bx, by := Corner(i, j)
-			cx, cy := Corner(i, j+1)
-			dx, dy := Corner(i+1,j+1)
-			fmt.Printf("<polygon points=%g,%g,%g,%g,%g,%g,%g,%g>\n",
-				ax,ay,bx,by,cx,cy,dx,dy)
-		}
-	}
-	fmt.Println("</svg>")
+	//fmt.Printf("<svg xmlns= 'http://www.32.org/2000/svg'" +
+	//	"style='stroke: grey; fill: white; stroke-width: 0.7'" +
+	//	"width='%d' height= '%d'>",width,height)
+	//for i := 0; i < cells; i++ {
+	//	for j := 0; j < cells; j++ {
+	//		ax, ay := Corner(i+1,j)
+	//		bx, by := Corner(i, j)
+	//		cx, cy := Corner(i, j+1)
+	//		dx, dy := Corner(i+1,j+1)
+	//		fmt.Printf("<polygon points=%g,%g,%g,%g,%g,%g,%g,%g>\n",
+	//			ax,ay,bx,by,cx,cy,dx,dy)
+	//	}
+	//}
+	//fmt.Println("</svg>")
+
+	//切片实现动态数组
+	var str = []string{"aaaa","bbbb"}
+	str = append(str, "cccc")
+	fmt.Println(str)
 }
 
 func Corner(i, j int) (float64, float64) {
--- tempconv/StirngFunc.go
+++ tempconv/StirngFunc.go
@@ -1,5 +1,7 @@
 package tempconv
 
+import "strings"
+
 //判断s字符串是否含有prefix的前缀
 func HasPrefix(s, prefix string) bool {
 	return len(s) >= len(prefix) && s[:len(prefix)] == prefix
@@ -18,4 +20,45 @@ func Contains(s, substr string) bool {
 		}
 	}
 	return false
+}
+
+//去掉像是路径的前缀和像是文件格式的后缀 a/b/c.go --> c
+func basename(s string) string {
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] =='/' {
+			s = s[i+1:]
+			break
+		}
+	}
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] =='.' {
+			s = s[:i]
+			break
+		}
+	}
+	return s
+}
+
+//调用string.LastIndex库函数
+func simpleBasename(s string) string {
+	//如果没有找到"/" 那么slash值为-1 否则返回“/”的位置下标
+	slash := strings.LastIndex(s, "/")
+	s = s[slash+1:]
+	if dot := strings.LastIndex(s, "."); dot >= 0 {
+		s = s[: dot]
+	}
+	return s
+}
+//[]string 字符串切片slice是不允许改变值得，  这是因为为了保护两个切片访问
+//同一哥底层string得时候，安全相同  但是是可以在中间加上字符得
+
+//没三个字符加上一个","
+func comma(s string) string {
+	n := len(s)
+	if n <= 3 {
+		return s
+	}
+
+	//递归
+	return comma(s[:n-3]+","+s[n-3:])
 }
