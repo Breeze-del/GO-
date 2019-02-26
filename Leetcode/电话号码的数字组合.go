a88956e62577026f5415d1a43b95a3e5a5101027
Liqingfeng <562916584@qq.com>
2019-02-26 21:53:00 +0800

    leetcode

--- Leetcode/电话号码的数字组合.go
+++ Leetcode/电话号码的数字组合.go
@@ -1,1 +1,21 @@
 package main
+
+var dd map[string]string
+
+func letterCombinations(digits string) []string {
+	return nil
+}
+
+func main() {
+	dd := make(map[string]string)
+	dd["1"] = ""
+	dd["2"] = "abc"
+	dd["3"] = "def"
+	dd["4"] = "ghi"
+	dd["5"] = "jkl"
+	dd["6"] = "mno"
+	dd["7"] = "pqs"
+	dd["8"] = "tuv"
+	dd["9"] = "wxyz"
+	letterCombinations("23")
+}
